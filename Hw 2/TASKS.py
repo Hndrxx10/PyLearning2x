bc5182057e88f5c80ac61de3cdd1d86a437273ba
David <davidiglesiasnov4@gmail.com>
2024-02-15 18:26:39 -0600

    07_Feb_2024 HW

---
+++ Hw 2/TASKS.py
@@ -0,0 +1,32 @@
+#Task #1
+#Explain the difference between the = operator and the == operator in Python.
+#What does the ** operator do in Python, and how is it used?
+#What does the ^ operator do in Python, and in what context is it commonly used?
+
+# 1. The = operator is used for assignment, while the == operator is used for comparison of equality.
+# 2. The ** operator is the exponentiation operator. It is used to raise the left operand to the power of the right operand.
+# 3. The ^ operator is the bitwise XOR (exclusive OR) operator. It performs a bitwise XOR operation on the operands, which means it returns 1 if the corresponding bits of the two operands are different, otherwise, it returns 0.
+
+
+
+#Task #2
+#Write a Python program to calculate the area of a circle given its radius using the formula area=_π_×_r^_2 ( Take pie as 3.14)
+#pi = 3.14
+#r = float(input("What is the radius?""\n"))
+#area_of_a_circle = pi*r**2
+#print("Area of the circle",area_of_a_circle)
+
+#Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+#num1 = float(input("Enter First Number"))
+#num2 = float(input("Enter Second Number"))
+#print("First Number is greater than Second Number", num1 > num2)
+#print("First Number is less than Second Number", num1 < num2)
+#print("First Number is equal to Second Number", num1 == num2)
+
+#Develop a Python script that calculates the square and cube of a given number.
+x = input("Enter The Number")
+x = int(x)
+square = x **2
+cube = x**3
+print(square)
+print(cube)
